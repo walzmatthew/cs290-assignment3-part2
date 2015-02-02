d047adae8e793c0aa8d519d221663af86657ec08
walzmatthew <walzma@onid.oregonstate.edu>
2015-02-01 18:02:28 -0800

    Committing final .js code with filter by language and multiple page search results implemented.

--- assignment3-part2.js
+++ assignment3-part2.js
@@ -32,51 +32,23 @@ function searchGitHub() {
   //clear the existing search results
   clearResults(resultsList);
   
-  //Send a GET request to GitHub
-  var GitHubRequest = new XMLHttpRequest();
-  GitHubRequest.open('GET', 'https://api.github.com/gists/public', true);
-  GitHubRequest.send();
-  GitHubRequest.onreadystatechange = alertContents;
+  //grab value from num_pages drop down <select> to run a loop of GET requests. Referenced below webpage for code: //http://stackoverflow.com/questions/1085801/get-selected-value-of-dropdownlist-using-javascript
+  var numPageSelect = document.getElementById("num_pages");
+  var numPages = numPageSelect.options[numPageSelect.selectedIndex].value;
+  var url = 'https://api.github.com/gists/public';
   
-  // //grab value from num_pages drop down <select>. Referenced below webpage for code: //http://stackoverflow.com/questions/1085801/get-selected-value-of-dropdownlist-using-javascript
-  // var numPageSelect = document.getElementById("num_pages");
-  // var numPages = numPageSelect.options[numPageSelect.selectedIndex].value;
+  for (var i = 1; i <= numPages; i++) { //Make a GET request for each page of results selected by user
+    //baseline url at beginning of each loop without page number
+    var url = 'https://api.github.com/gists/public?page=';
+    //append url with appropriate page number. NOTE: i starts at 1 so simply add i
+	url += i;
   
-  // //create variables for checking if new link is already in favorites
-  // var inFavs = false;
-  // var string = "string"
-  
-  // for (var i = 0; i < numPages; i++) {
-    // //check if new link is already in favorites
-	// inFavs = false;
-    // string = "http://www.google.com" + i + "/";
-    // for (var j = 0; j < localStorage.length; j++) {
-	  // if (string == localStorage.key(j)) {
-	    // inFavs = true;
-	  // }
-	// }
-	// if (inFavs == false) {
-      // //create new dt elements, text nodes with the text content for the dts, and hyperlinks
-      // var listItem = document.createElement("dt");
-      // var itemLink = document.createElement("a");
-      // itemLink.href = "http://www.google.com" + i;
-
-      // var itemText = document.createTextNode("item #" + i);
-
-      // //create a button to append to the dt which calls addToFavs when clicked. Help for how to wrap the button command to pass arguments from:
-      // // http://stackoverflow.com/questions/14034737/creating-a-button-dynamically-with-arguments-for-onclick
-      // var addToFavsButt = document.createElement('input');
-      // addToFavsButt.setAttribute('type', 'button');
-      // addToFavsButt.setAttribute('value', 'Add Gist to Favorites');
-  
-      // //append the text node to the hyperlink, then append the hyperlink and button to the dt, then append the dt to the dl
-      // itemLink.appendChild(itemText);
-      // listItem.appendChild(itemLink);
-      // addToFavsButt.onclick = function() {addToFavs(this.parentElement)};
-      // listItem.appendChild(addToFavsButt);
-      // resultsList.appendChild(listItem);
-    // }
-  // }
+    //Send a GET request to GitHub
+    var GitHubRequest = new XMLHttpRequest();
+    GitHubRequest.open('GET', url, true);
+    GitHubRequest.send();
+    GitHubRequest.onreadystatechange = alertContents;
+  }
 }  
   
 function clearResults(results) {
@@ -171,7 +143,29 @@ function createResultList(array) { //create dt's using the parsed content from G
   var inFavs = false;
   var k = 1;
   
-  for (var i = 0; i < 90; i+=3) {
+  //create variable to check if filters are needed and if so, create an array of language filters
+  var languagesSelected = document.getElementsByTagName("input");
+  var filters = new Array();
+  var filterByLanguage = true;
+  if ((languagesSelected[0].checked == false) && (languagesSelected[1].checked == false) && (languagesSelected[2].checked == false) && (languagesSelected[3].checked == false)) { //No filters selected
+    filterByLanguage = false;
+  }
+  else { //language filters selected
+	if (languagesSelected[0].checked == true) {
+	  filters.push('Python');
+	}
+    if (languagesSelected[1].checked == true) {
+	  filters.push('JSON');
+	}
+	if (languagesSelected[2].checked == true) {
+	  filters.push('JavaScript');
+	}
+	if (languagesSelected[3].checked == true) {
+	  filters.push('SQL');
+	}
+  }	
+	
+  for (var i = 0; i < 90; i+=3) { 
     //check if new link is already in favorites
 	inFavs = false;
 	k = 1;
@@ -182,23 +176,52 @@ function createResultList(array) { //create dt's using the parsed content from G
 	  }
 	}
 	if (inFavs == false) { //if not already in favorites create new dt elements, text nodes with the descriptions for the dts, and hyperlinks
-      var listItem = document.createElement("dt");
-      var itemLink = document.createElement("a");
-      itemLink.href = array[i];
-      var itemText = document.createTextNode(array[k]);
+	
+	  //check if language meets the appropriate filters
+	  if (filterByLanguage == true) { //at least one checkbox was checked
+	    var languageIndex = 2;
+		languageIndex += i;
+	    for (var l = 0; l < filters.length; l++) { //loop through elements in filters array
+		  if (array[languageIndex] == filters[l]) { //language matches filter so create new item for Results list
+		    var listItem = document.createElement("dt");
+            var itemLink = document.createElement("a");
+            itemLink.href = array[i];
+            var itemText = document.createTextNode(array[k]);
 
-      //create a button to append to the dt which calls addToFavs when clicked. Help for how to wrap the button command to pass arguments from:
-      // http://stackoverflow.com/questions/14034737/creating-a-button-dynamically-with-arguments-for-onclick
-      var addToFavsButt = document.createElement('input');
-      addToFavsButt.setAttribute('type', 'button');
-      addToFavsButt.setAttribute('value', 'Add Gist to Favorites');
+            //create a button to append to the dt which calls addToFavs when clicked. Help for how to wrap the button command to pass arguments from:
+            // http://stackoverflow.com/questions/14034737/creating-a-button-dynamically-with-arguments-for-onclick
+            var addToFavsButt = document.createElement('input');
+            addToFavsButt.setAttribute('type', 'button');
+            addToFavsButt.setAttribute('value', 'Add Gist to Favorites');
   
-      //append the text node to the hyperlink, then append the hyperlink and button to the dt, then append the dt to the dl
-      itemLink.appendChild(itemText);
-      listItem.appendChild(itemLink);
-      addToFavsButt.onclick = function() {addToFavs(this.parentElement)};
-      listItem.appendChild(addToFavsButt);
-      resultsList.appendChild(listItem);
+            //append the text node to the hyperlink, then append the hyperlink and button to the dt, then append the dt to the dl
+            itemLink.appendChild(itemText);
+            listItem.appendChild(itemLink);
+            addToFavsButt.onclick = function() {addToFavs(this.parentElement)};
+            listItem.appendChild(addToFavsButt);
+            resultsList.appendChild(listItem);
+	      }
+		}
+      }
+	  else { //no checkboxes checked so no need to check filter. Just create new item for Results list. Code identical to above code for scenario when filters are passed.
+        var listItem = document.createElement("dt");
+        var itemLink = document.createElement("a");
+        itemLink.href = array[i];
+        var itemText = document.createTextNode(array[k]);
+
+        //create a button to append to the dt which calls addToFavs when clicked. Help for how to wrap the button command to pass arguments from:
+        // http://stackoverflow.com/questions/14034737/creating-a-button-dynamically-with-arguments-for-onclick
+        var addToFavsButt = document.createElement('input');
+        addToFavsButt.setAttribute('type', 'button');
+        addToFavsButt.setAttribute('value', 'Add Gist to Favorites');
+  
+        //append the text node to the hyperlink, then append the hyperlink and button to the dt, then append the dt to the dl
+        itemLink.appendChild(itemText);
+        listItem.appendChild(itemLink);
+        addToFavsButt.onclick = function() {addToFavs(this.parentElement)};
+        listItem.appendChild(addToFavsButt);
+        resultsList.appendChild(listItem);
+	  }
     }
   }
 }
